6b95c6bd034792b69736901f3b004cd22968b9a7
bruhtus <hibruhtus@gmail.com>
2020-11-07 09:50:56 +0700

    First attempt

---
+++ instasaver.py
@@ -0,0 +1,76 @@
+import re
+import os
+import tempfile
+import instaloader
+import base64
+import streamlit as st
+
+from fire import Fire
+from zipfile import ZipFile
+
+def main():
+    st.title('Instasaver')
+    st.markdown('Only for public profile')
+    with tempfile.TemporaryDirectory() as temp:
+        load = instaloader.Instaloader(
+                dirname_pattern=temp,
+                #filename_pattern='saved',
+                download_comments=False,
+                download_geotags=False,
+                download_video_thumbnails=False,
+                save_metadata=False)
+
+        url_input = st.text_input('Post Url')
+        
+        try:
+            if url_input:
+                load.post_metadata_txt_pattern = ''
+                shortcode = url_to_short_code(url_input)
+                check_post_url(load, shortcode, temp)
+        
+        except AttributeError:
+            st.write("It seems like it's not a url")
+
+        except KeyError:
+            st.write("It seems like it's a private account")
+
+def url_to_short_code(post_url):
+    regexp = '^(?:.*\/(p|tv)\/)([\d\w\-_]+)'
+    post_short_code = re.search(regexp, post_url).group(2)
+    print('From url {} extracted shorcode:{}'.format(post_url, post_short_code))
+    print(f'shotcode: {post_short_code}')
+    return post_short_code
+
+def check_post_url(loader, shortcode, temp):
+    post = instaloader.Post.from_shortcode(loader.context, shortcode)
+    loader.download_post(post, target=temp)
+    file_list = [filename for filename in os.listdir(temp)]
+    #print(len(file_list))
+    if len(file_list) == 1:
+        try:
+            st.image(f'{temp}/{file_list[0]}', use_column_width=True)
+            st.markdown(download_button(f'{temp}/{file_list[0]}', temp), unsafe_allow_html=True)
+
+        except:
+            st.video(f'{temp}/{file_list[0]}')
+            st.markdown(download_button(f'{temp}/{file_list[0]}', temp), unsafe_allow_html=True)
+
+    else:
+        for filename in file_list:
+            try:
+                st.image(f'{temp}/{filename}', use_column_width=True)
+                st.markdown(download_button(f'{temp}/{filename}', temp), unsafe_allow_html=True)
+
+            except:
+                st.video(f'{temp}/{filename}')
+                st.markdown(download_button(f'{temp}/{filename}', temp), unsafe_allow_html=True)
+
+def download_button(bin_file, temp):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+        bin_str = base64.b64encode(data).decode()
+        href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download here</a>'
+        return href
+
+if __name__ == '__main__':
+    Fire(main)
---
+++ readme.md
@@ -0,0 +1,14 @@
+## Instasaver
+> This is just-for-fun project to save memes on instagram (hopefully only that).
+
+### Using instaloader and streamlit python module
+Combine instaloader as back-end and steamlit as front-end then, **boom**, you become a ~~a stalker~~ *ehem i mean*, **meme dealer**.
+
+Jokes aside, this is just a project to implement a user friendly interface using streamlit for instaloader python module (kind of) and use it without a hassle (hopefully).
+
+#### References
+- [The one who trigger me to made this](https://instasave.egoist.sh/).
+- [Instaloader documentations](https://instaloader.github.io/as-module.html).
+- [Streamlit](https://www.streamlit.io/).
+- [View public profile anonymous (more advance version)](https://insta-stories.ru/).
+- [Streamlit download file](https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806/23).
